fb0b3df7834a67e6e7b69181b4e722814212519b
Your Name <you@example.com>
2016-07-29 17:48:22 +0300

    wewewe

---
+++ GetLucky/js/TempatesDirective.js
@@ -0,0 +1,28 @@
+﻿let path = '../views/templates/';
+
+myApp.directive('mainNavBar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: path + 'mainNavBar.html',
+        replace: true,
+        transclude: true,
+    }
+})
+
+myApp.directive('blogNavBar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: path + 'blogNavBar.html',
+        replace: true,
+        transclude: true,
+    }
+})
+
+myApp.directive('createPostForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: path + 'createPostForm.html',
+        replace: true,
+        transclude: true,
+    }
+})
